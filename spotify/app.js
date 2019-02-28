9a36daad18da5ad71e686e95b93802779b11529a
franlol <fraaaaan@gmail.com>
2019-02-28 17:19:50 +0100

    showing albums, css left

--- spotify/app.js
+++ spotify/app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const SpotifyWebApi = require('spotify-web-api-node');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,13 +23,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// catch 404 and forward to error handler--------------------------------------------------------------
+app.use((req, res, next) => {
   next(createError(404));
 });
-
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,4 +38,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(3000, () => {
+  console.log('Server listening port 3000');
+});
--- spotify/routes/index.js
+++ spotify/routes/index.js
@@ -1,9 +1,53 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const SpotifyWebApi = require('spotify-web-api-node');
+
+// Remember to insert your credentials here----------------------
+const clientId = '2b206eb022fa4e3cab91cc85b270fc6f';
+const clientSecret = '403cb0f884164b7692efda09a1952ea1';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
+});
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
+// data.body.artists.items
+router.get('/artists', async (req, res, next) => {
+  const { search } = req.query;
+  try {
+    const data = await spotifyApi.searchArtists(search);
+    console.log(data.body.artists.items[0].images[0].url);
+
+    res.render('artists', { artists: data.body.artists.items }); // { data: data }
+  } catch (error) {
+    console.log('The error while searching artists occurred: ', error);
+  }
+});
+
 module.exports = router;
+
+// { external_urls:
+//   { spotify: 'https://open.spotify.com/artist/1YEGETLT2p8k97LIo3deHL' },
+//  followers: { href: null, total: 121393 },
+//  genres: [ 'canadian rock', 'pop rock' ],
+//  href: 'https://api.spotify.com/v1/artists/1YEGETLT2p8k97LIo3deHL',
+//  id: '1YEGETLT2p8k97LIo3deHL',
+//  images: [ [Object], [Object], [Object], [Object] ],
+//  name: 'Crash Test Dummies',
+//  popularity: 55,
+//  type: 'artist',
+//  uri: 'spotify:artist:1YEGETLT2p8k97LIo3deHL' },

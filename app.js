4131a6e50806b6ee67b47f6d727eb8416d968603
Yvonne Moser <yvonne_moser@outlook.de>
2019-04-28 19:50:57 +0200

    css without table

--- app.js
+++ app.js
@@ -46,16 +46,42 @@ app.get(`/artists`, (req,res,) => {
   console.log(req.query)
   spotifyApi.searchArtists(req.query.text)
   .then(data => {
-    console.log(data);
-    console.log("The received data from the API: ", data.body);
+    console.log(data.body.artists.items[0]);
+    console.log(data.body.artists.items[0].id);
+    //console.log("The received data from the API: ", data.body);
     res.render(`artistsFile`, data.body.artists)
+   
+   
   })
   .catch(err => {
     console.log("The error while searching artists occurred: ", err);
   })
 });
- 
 
+ 
+app.get(`/artistsFile/:id/albums`, (req, res) => {
+  console.log(req.params);
+  spotifyApi.getArtistAlbums(req.params.id)
+  .then(data => {
+    console.log(data.body.items[0]);
+    res.render(`albums`, data.body);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+});
+
+app.get(`/albums/:tracks`, (req, res) => {
+  console.log(req.params);
+  spotifyApi.getAlbumTracks(req.params.tracks, { limit : 5, offset : 1 })
+  .then(data => {
+    console.log(data.body.items);
+    res.render(`tracks`, data.body);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+});
 
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));

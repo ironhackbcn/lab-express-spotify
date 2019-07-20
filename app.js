7ded7462eb618c034a4fd8f57f94ab29d558f768
Gloria Visus <gloriavisus@gmail.com>
2019-07-20 17:10:01 +0200

    get-albums

--- app.js
+++ app.js
@@ -6,31 +6,14 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const hbs = require('hbs');
 
-// require spotify-web-api-node package here:
-const SpotifyWebApi = require('spotify-web-api-node');
-const clientId = 'ea338f7fdc3845edbe4444ce7ece8947';
-const clientSecret = '8aaf65a42be641dab99f2e6055b476e4';
-
-const spotifyApi = new SpotifyWebApi({
-  clientId: clientId,
-  clientSecret: clientSecret
-});
 const indexRouter = require('./routes/index');
 const artistRouter = require('./routes/artists');
+
 const app = express();
 
 // esto es cuando usuario entra en homepage ira al index y ejecutará indexRouter
 app.use('/', indexRouter);
 app.use('/artists', artistRouter);
-
-// Retrieve an access token
-spotifyApi.clientCredentialsGrant()
-  .then(data => {
-    spotifyApi.setAccessToken(data.body['access_token']);
-  })
-  .catch(error => {
-    console.log('Something went wrong when retrieving an access token', error);
-  });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

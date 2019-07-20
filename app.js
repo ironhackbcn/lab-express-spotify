b5eb8c5d7ee7519b65c06903e4798197c2fcdc64
Anna Trogimova <cobrana1000@gmail.com>
2019-07-20 14:04:13 +0200

    my first commit

--- app.js
+++ app.js
@@ -5,38 +5,54 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const hbs = require('hbs');
 
-// require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
+const clientId = '7dcadd78f00f4829b8495817508312c0';
+const clientSecret = '8c9a6b3bd78a4d49a6fa00342dcb6c29';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
+});
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  });
 
 const indexRouter = require('./routes/index');
 
- const app = express();
+const app = express();
 
- // view engine setup
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
- app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
- app.use('/', indexRouter);
+app.use('/', indexRouter);
 
- // catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
 
- // error handler
+// error handler
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-   // render the error page
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
- module.exports = app;
+module.exports = app;

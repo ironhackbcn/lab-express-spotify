79c301a5fa7f2faae4604a90d8bb3649ff48283f
Carlos Muñoz <carlosmunyozguimera@gmail.com>
2019-07-20 18:23:17 +0200

    connection with Sportify API

--- app.js
+++ app.js
@@ -6,6 +6,24 @@ const logger = require('morgan');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const clientId = '854da551673f443782ee09f22c0b2461';
+const clientSecret = 'bc6a688345cc40c6b71a9cb9c7014f8a';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
+});
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  });
 
 const indexRouter = require('./routes/index');
 

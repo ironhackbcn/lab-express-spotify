ed262a3ae1af5e3d96e84a691fad44e93a12f467
Gloria Visus <gloriavisus@gmail.com>
2019-07-20 14:04:19 +0200

    router artists

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const createError = require('http-errors');
+'use strict';
+
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -6,37 +7,57 @@ const logger = require('morgan');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
+const clientId = 'ea338f7fdc3845edbe4444ce7ece8947';
+const clientSecret = '8aaf65a42be641dab99f2e6055b476e4';
 
+const spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
+});
 const indexRouter = require('./routes/index');
+const artistRouter = require('./routes/artists');
+const app = express();
 
- const app = express();
+// esto es cuando usuario entra en homepage ira al index y ejecutará indexRouter
+app.use('/', indexRouter);
+app.use('/artists', artistRouter);
 
- // view engine setup
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  });
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
- app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
- app.use('/', indexRouter);
-
- // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+  res.status(404);
+  res.render('not-found');
 });
 
- // error handler
+// NOTE: requires a views/error.ejs template
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
 
-   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500);
+    res.render('error');
+  }
 });
 
- module.exports = app;
+module.exports = app;
--- routes/artists.js
+++ routes/artists.js
@@ -3,8 +3,10 @@
 const express = require('express');
 const router = express.Router();
 
+const SpotifyWebApi = require('spotify-web-api-node');
+
 router.get('/', (req, res, next) => {
-  res.render('index.hbs', { title: 'Spotify App' });
+  res.render('artists');
 });
 
 module.exports = router;

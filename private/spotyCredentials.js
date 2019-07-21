25f7a04a1cb55cdffaa565e52ed50c7046367a10
Esther Bernal <esther.bernal.lopez@gmail.com>
2019-07-21 21:05:16 +0200

    credenciales privadas

--- private/spotyCredentials.js
+++ private/spotyCredentials.js
@@ -2,10 +2,6 @@
 // API connection
 const SpotifyWebApi = require('spotify-web-api-node');
 const credentials = require('./credentials');
-/*
-const clientId = '58206d77dca14d329b64d6d74b1d5a7d';
-const clientSecret = '968837a0664f4747b145e2bcd8e69d74';
-*/
 
 const clientId = credentials.clientId;
 const clientSecret = credentials.clientSecret;

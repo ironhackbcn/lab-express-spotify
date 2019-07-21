3a8c99992a5021b502325a306ba905114079a159
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-07-21 15:48:04 +0200

    work in artist search

--- routes/artists.js
+++ routes/artists.js
@@ -3,8 +3,34 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.render('artists');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+// Remember to insert your credentials here
+const clientId = 'fed8cc90df634455a7c3607e0df63492';
+const clientSecret = '67891505eda04165a28035e25f1c78a9';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
+});
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  });
+
+router.get('/', async (req, res, next) => {
+  const { artist } = req.query;
+  try {
+    const artistArray = await spotifyApi.searchArtists(artist);
+    res.render('artists', { artist: artistArray.body.artists.items });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;

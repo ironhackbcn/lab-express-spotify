b46c5a31c1a76a3ec4dca1e2af51fb4ff92bf791
Esther Bernal <esther.bernal.lopez@gmail.com>
2019-07-21 20:49:33 +0200

    credenciales privadas

---
+++ private/spotyCredentials.js
@@ -0,0 +1,21 @@
+'use strict';
+// API connection
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const clientId = '58206d77dca14d329b64d6d74b1d5a7d';
+const clientSecret = '968837a0664f4747b145e2bcd8e69d74';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
+});
+
+spotifyApi.clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  });
+
+module.exports = spotifyApi;
--- routes/albums.js
+++ routes/albums.js
@@ -2,25 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-
-// API connection
-const SpotifyWebApi = require('spotify-web-api-node');
-
-const clientId = '58206d77dca14d329b64d6d74b1d5a7d';
-const clientSecret = '968837a0664f4747b145e2bcd8e69d74';
-
-const spotifyApi = new SpotifyWebApi({
-  clientId: clientId,
-  clientSecret: clientSecret
-});
-
-spotifyApi.clientCredentialsGrant()
-  .then(data => {
-    spotifyApi.setAccessToken(data.body['access_token']);
-  })
-  .catch(error => {
-    console.log('Something went wrong when retrieving an access token', error);
-  });
+const spotifyApi = require('../private/spotyCredentials');
 
 router.get('/:artistId', async (req, res, next) => {
   try {

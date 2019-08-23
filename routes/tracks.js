c6904a38ccd2cc718683678539f992c5d491c7b7
JuanVicenteVazquezG <juanvicentevazquezg@gmail.com>
2019-08-23 16:57:56 +0200

    "all finished but not pretty"

--- routes/tracks.js
+++ routes/tracks.js
@@ -8,7 +8,14 @@ const spotifyApi = require('../spotifypass/spotifypass');
 
 router.get('/:id', (req, res, next) => {
   const { id } = req.params;
-  console.log(id);
+  spotifyApi.getAlbumTracks(id)
+    .then((data) => {
+      const { items } = data.body;
+      res.render('tracks', { items });
+    })
+    .catch((error) => {
+      console.log(`The error while searching artists occurred: ${error}`);
+    });
 });
 
 

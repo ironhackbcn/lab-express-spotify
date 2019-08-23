9adf331affe4716650b79c007c9f5dfad54afcaa
Juan Vicente Vazquez G <juanvicentevazquezg@gmail.com>
2019-08-23 07:01:27 +0200

    artist almost done

--- app.js
+++ app.js
@@ -4,29 +4,14 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const hbs = require('hbs');
-const SpotifyWebApi = require('spotify-web-api-node');
-
-// Remember to insert your credentials here
-const clientId = 'efe0d3afa92b41ed86c489f0f3d8db60';
-const clientSecret = 'bcba6f256e7f4c0da738a2037aa191cf';
-
-const spotifyApi = new SpotifyWebApi({
-  clientId : clientId,
-  clientSecret : clientSecret
-});
 
 
 
-// Retrieve an access token
-spotifyApi.clientCredentialsGrant()
-  .then( data => {
-    spotifyApi.setAccessToken(data.body['access_token']);
-  })
-  .catch(error => {
-    console.log('Something went wrong when retrieving an access token', error);
-  })
+
+
 
 const indexRouter = require('./routes/index');
+const artistsRouter = require('./routes/artists');
 
  const app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
 const router = express.Router();
 
- router.get('/', (req, res, next) =>{
-  res.render('index', {title: 'Express'});
+router.get('/', (req, res, next) => {
+  res.render('index');
 });
 
- module.exports = router;
+
+
+module.exports = router;

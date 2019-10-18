79868cdf3da76361c4fca45f1e82c8ee28f25176
Daniel Duque <duque@outlook.com>
2019-10-18 15:07:14 +0200

    Finished file structure, api calling and server activation

--- app.js
+++ app.js
@@ -5,38 +5,61 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const hbs = require('hbs');
 
-// require spotify-web-api-node package here:
+// BEGIN SPOTIFY INTEGRATION
+const SpotifyWebApi = require('spotify-web-api-node');
+const clientId = '272ee2be75504c7ea7e76fb1e582a6c8',
+  clientSecret = '6d2878a851f3439b8042d0169c4dfa4e';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
+});
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
+// END SPOTIFY INTEGRATION
 
 const indexRouter = require('./routes/index');
 
- const app = express();
+const app = express();
 
- // view engine setup
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
- app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
- app.use('/', indexRouter);
+app.use('/', indexRouter);
 
- // catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
 
- // error handler
+// Error handler
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-   // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
  module.exports = app;
+
+ // Start server in port 3000
+ app.listen(3000, () => {
+   console.log('Server listening on port 3000')
+ });

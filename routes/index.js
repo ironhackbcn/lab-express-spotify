5bffebaa9283bd4de26157058efc35018107cf3c
Daniel Duque <duque@outlook.com>
2019-10-19 10:09:19 +0200

    Iteration 5 finished

--- routes/index.js
+++ routes/index.js
@@ -2,36 +2,58 @@ const express = require('express');
 const spotifyApi = require('../spotifyApi'); // Require SpotifyApi
 const router = express.Router();
 
-// Load Index view
+// Index view
 router.get('/', (req, res, next) =>{
   res.render('index', {title: 'Spotify Lab'});
 });
 
-// Load Artists view
+// Artists view
 router.get('/artists', (req, res) => {
   let {artist} = req.query;
   spotifyApi.searchArtists(artist)
     .then(data => {
       let {items} = data.body.artists;
-      res.render('artists', {items});
+      res.render('artists', {
+        items,
+        title: 'Artists'
+      });
     })
     .catch(error => {
       console.log(`An error ocurred: ${error}`);
     })
 });
 
-// Load Albums view
-router.get('/albums/:id', (req, res, next) => {
-  let {id} = req.params;
-  spotifyApi.getArtistAlbums(id)
+// Albums view
+router.get('/albums/:artistId', (req, res) => {
+  let {artistId} = req.params;
+  spotifyApi.getArtistAlbums(artistId)
     .then(data => {
       let {items} = data.body;
-      console.log(items);
-      res.render('albums', {items});
+      res.render('albums', {
+        items,
+        title: 'Albums'
+      });
     })
     .catch(error => {
-      console.log(`An error ocurred: ${error}`);
+      console.log(`An error occurred: ${error}`);
     })
-})
+});
+
+// Tracks View
+router.get('/tracks/:albumId', (req, res) => {
+  let {albumId} = req.params;
+  spotifyApi.getAlbumTracks(albumId)
+    .then(data => {
+      let {items} = data.body;
+      console.log(items)
+      res.render('tracks', {
+        items,
+        title: 'Tracks'
+      });
+    })
+    .catch(error => {
+      console.log(`An error occurred: ${error}`);
+    })
+});
 
  module.exports = router;

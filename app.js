5e52068afea80a711673c74dc1c36572e2623c64
noemi alonso <noemialonso@MacBook-Pro-de-noemi.local>
2019-10-22 19:47:04 +0200

    commit despues de tirar agua en el portatil

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const indexRouter = require('./routes/index');
  // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
 
  app.use(logger('dev'));
 app.use(express.json());
@@ -21,7 +22,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
  app.use('/', indexRouter);
+
 
  // catch 404 and forward to error handler
 app.use((req, res, next) => {
